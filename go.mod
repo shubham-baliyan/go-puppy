536726f6cc4388a5b3250677e9a5e5af5ba18416
shubham baliyan <shubham.baliyan204@gmail.com>
2023-07-17 10:33:13 +0100

    add dep of  dog

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/shubham-baliyan/go-puppy
 
 go 1.19
+
+require github.com/shubham-baliyan/go-dog v0.0.0-20230717090357-87a4d9042a6b
--- puppy.go
+++ puppy.go
@@ -1,8 +1,16 @@
 package puppy
 
+import (
+	"github.com/shubham-baliyan/go-dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 func Barks() string {
 	return "Woof Woof Woof !!!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Barks())
 }
